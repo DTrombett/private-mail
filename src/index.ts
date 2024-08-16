5e9c9da0d055873503f9bc242132a9c5f1ea892e
DTrombett <maxtromb.dt@gmail.com>
2024-08-16 15:28:00 +0200

    feat: implement custom email

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,55 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import PostalMime from "postal-mime";
+import { Resend } from "resend";
+
+const parser = new PostalMime();
+const streamToArrayBuffer = async (
+	stream: ReadableStream<Uint8Array>,
+	size: number
+) => {
+	let result = new Uint8Array(size);
+	let bytesRead = 0;
+	const reader = stream.getReader();
+
+	while (true) {
+		const { done, value } = await reader.read();
+
+		if (done) break;
+		result.set(value, bytesRead);
+		bytesRead += value.length;
+	}
+	return result;
+};
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	email: async (message, env) => {
+		if (message.from !== env.FROM_EMAIL)
+			return message.forward(env.FORWARD_EMAIL);
+		const rawEmail = await streamToArrayBuffer(message.raw, message.rawSize);
+		const parsedEmail = await parser.parse(rawEmail);
+		const [to, ...subject] = parsedEmail.subject!.split(" ");
+		const resend = new Resend(env.RESEND_API_KEY);
+		const res = await resend.emails.send({
+			from: message.to,
+			to,
+			subject: subject.join(" "),
+			html: parsedEmail.html!,
+			text: parsedEmail.text!,
+			attachments: parsedEmail.attachments.map((att) => ({
+				content: Buffer.from(att.content),
+				content_type: att.mimeType,
+				filename: att.filename!,
+			})),
+			headers: { date: parsedEmail.date! },
+		});
+
+		if (res.error) {
+			message.setReject(res.error.message);
+			throw new Error(res.error.message);
+		}
+		message.setReject("Correctly forwarded");
+	},
+	fetch: async (req) => {
+		console.log(req.cf);
+		return new Response(null, { status: 404 });
 	},
 } satisfies ExportedHandler<Env>;
